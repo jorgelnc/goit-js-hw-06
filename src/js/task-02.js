bdeb566c3f0106f22b01c7719fbadc5ab7ab06f4
jorgelnc <jorgenavacolina@gmail.com>
2023-06-13 17:42:33 -0500

    modifying task 2 code

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,3 +1,10 @@
+// Escriba un script que para cada elemento del array ingredients:
+
+// Cree un elemento separado <li>. Asegúrese de usar el método document.createElement().
+// Añada el nombre de un ingrediente como contenido de texto.
+// Añada una clase item al elemento.
+// Después, inserte todos los <li> en una sola operación en la lista ul#ingredients.
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +13,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+ingredients.forEach(ingredient => {
+ let ingredientList = document.createElement("li");
+  ingredientList.textContent = ingredient;
+  ingredientList.classList.add("item");
+ console.log(ingredientList)
+  });
+
+
+
