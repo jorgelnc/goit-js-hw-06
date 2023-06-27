4122ee89ca8e9ed1e2617f850bbe87ae1380dca7
jorgelnc <jorgenavacolina@gmail.com>
2023-06-27 10:33:53 -0500

    task 8 added

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,38 @@
+// Escriba un script para controlar el formulario de acceso.
+
+// <form class="login-form">
+//   <label>
+//     Email
+//     <input type="email" name="email" />
+//   </label>
+//   <label>
+//     Password
+//     <input type="password" name="password" />
+//   </label>
+//   <button type="submit">Login</button>
+// </form>
+
+// El procesamiento de form.login-form debe ser por el evento submit.
+// La página no debe recargarse cuando se envía el formulario.
+// Si hay espacios en blanco en el formulario, aparecerá alert con una advertencia diciendo que todos los espacios deben ser rellenados.
+// Si el usuario ha rellenado todos los espacios y ha enviado el formulario, reúne los valores de los espacios en donde el nombre del espacio 
+// es el nombre de la propiedad y el valor del espacio es la propiedad.Use la propiedad elements para acceder a los elementos del formulario.
+// Muestre el objeto con los datos introducidos en la consola y borre los valores de los camposespacio del formulario usando el método reset.
+
+
+const mainForm = document.querySelector(".login-form");
+
+mainForm.addEventListener("submit", (e) => {
+   e.preventDefault();
+   let inputMail = e.target[0].value.trim();
+   let inputPass = e.target[1].value.trim();
+   if (inputMail === ("") || inputPass === ("")) {
+      alert("Todos los espacios deben ser completados");
+   } else {
+      const results = {email : inputMail, password: inputPass};
+       mainForm.reset();
+      alert("Enviando Datos");
+      console.log(results);
+    }
+});
 
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,20 @@
+// Escriba un script que cambie el color de fondo del elemento <body>
+//  mediante el estilo inline cuando se hace clic en button.change - color y emite el valor del color en span.color.
+
+// <div class="widget">
+//   <p>Background color: <span class="color">-</span></p>
+//   <button type="button" class="change-color">Change color</button>
+// </div>
+
+// Use la función getRandomHexColor para generar un color aleatorio.
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
